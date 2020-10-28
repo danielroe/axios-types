a5d76a3f622fab13dfe9d78c6a0abcb2644b7519
Daniel Roe <daniel@roe.dev>
2020-10-28 09:40:35 +0000

    chore: init

---
+++ nuxt.config.js
@@ -0,0 +1,13 @@
+export default {
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+}
